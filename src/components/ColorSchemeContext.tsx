dbdbb5e948ff680eed6b7ef8c11afab94a5d3d97
kibecodes <106477223+kibecodes@users.noreply.github.com>
2023-11-01 15:31:58 +0300

    switched to contextAPI for state management

---
+++ src/components/ColorSchemeContext.tsx
@@ -0,0 +1,63 @@
+import { StatusBar } from "expo-status-bar";
+import React, { ReactNode, createContext, useCallback, useContext, useReducer } from "react";
+import { Appearance, Dimensions, View } from "react-native";
+
+export type ColorSchemeName = "dark" | "light";
+
+interface ColorScheme {
+    colorScheme: ColorSchemeName;
+}
+
+interface ColorSchemeContext extends ColorScheme {
+    dispatch: (scheme: ColorScheme) => void;
+}
+
+//** sets an initial value for the colorScheme context */
+const defaultValue: ColorScheme = {
+    colorScheme: Appearance.getColorScheme() ?? "light"
+}
+
+const ColorSchemeContext = createContext<ColorSchemeContext | null>(null);
+
+//**reducer func - takes initial state and action of the same types */
+const colorSchemeReducer = (_ : ColorScheme, colorScheme: ColorScheme) => {
+    return colorScheme;
+}
+
+export const useColorScheme = () => {
+    const ctx = useContext(ColorSchemeContext);
+    if(ctx === null) {
+        throw new Error("No ColorScheme context found");
+    }
+    const { colorScheme, dispatch } = ctx;
+    const toggle = useCallback(async () => {
+        const newColorScheme = colorScheme === "light" ? "dark" : "light";
+        dispatch({
+            colorScheme: newColorScheme
+        });
+    }, [colorScheme, dispatch]);
+    return {colorScheme, toggle}
+}
+
+interface ColorSchemeProviderProps {
+    children: ReactNode;
+}
+
+const { width, height } = Dimensions.get("window");
+
+export const ColorSchemeProvider = ({children}: ColorSchemeProviderProps) => {
+    const [ {colorScheme}, dispatch ] = useReducer(colorSchemeReducer, defaultValue)
+    return (
+        <View style={{ flex: 1 }}>
+            <StatusBar style={ colorScheme === "light" ? "dark" : "light" }/>
+            <View style={{ flex: 1 }}>
+                <ColorSchemeContext.Provider value={{
+                    colorScheme,
+                    dispatch
+                }}>
+                    {children}
+                </ColorSchemeContext.Provider>
+            </View>
+        </View>
+    )
+}
--- src/components/theme.ts
+++ src/components/theme.ts
@@ -1,11 +1,66 @@
-import { useSelector } from 'react-redux';
+import {
+  createTheme,
+  createBox,
+  createText,
+  useTheme as useThemeRS,
+} from '@shopify/restyle';
 
-import { darkTheme, lightTheme } from '../../redux/constants/themeConstants';
-import { selectThemeMode } from '../../redux/slices/themeSlice';
+import { ColorSchemeName } from './ColorSchemeContext';
 
-export function useTheme() {
-  const themeMode = useSelector(selectThemeMode);
-  const theme = themeMode === 'dark' ? darkTheme : lightTheme;
+const palette = {
+  black: '#0B0B0B',
+  white: '#F0F2F3',
+  lightGray: 'rgba(0, 0, 0, 0.04)',
+  darkGray: '#3B3B3B',
+};
 
-  return theme;
-}
+export const theme = createTheme({
+  colorScheme: 'light' as ColorSchemeName,
+  colors: {
+    mainBackground: palette.white,
+    mainForeground: palette.black,
+    secondaryBackground: palette.lightGray,
+  },
+  spacing: {
+    s: 8,
+    m: 16,
+    l: 24,
+    xl: 40,
+  },
+  textVariants: {
+    defaults: {
+      color: 'mainForeground',
+    },
+    header: {
+      fontWeight: 'bold',
+      fontSize: 30,
+      fontFamily: 'SFProDisplayBold',
+    },
+    item: {
+      fontWeight: 'bold',
+      fontSize: 16,
+      fontFamily: 'SFProDisplayBold',
+    },
+    body: {
+      fontSize: 16,
+      lineHeight: 24,
+    },
+  },
+});
+
+export type Theme = typeof theme;
+
+export const darkTheme: Theme = createTheme({
+  ...theme,
+  colorScheme: 'dark',
+  colors: {
+    ...theme.colors,
+    mainBackground: palette.black,
+    mainForeground: palette.white,
+    secondaryBackground: palette.darkGray,
+  },
+});
+
+export const box = createBox<Theme>();
+export const text = createText<Theme>();
+export const useTheme = useThemeRS<Theme>;
