0ed203a91a3e478a5f9d11f35311612d9a275e11
Jonathan <106477223+kibecodes@users.noreply.github.com>
2023-12-12 22:30:31 +0300

    Update apiTest.tsx

--- src/components/__tests__/apiTest.tsx
+++ src/components/__tests__/apiTest.tsx
@@ -1,3 +1,30 @@
-test("API endpoint reaches the server", () => {
+test("should fetch data from the API", () => {
   
 })
+
+const nock = require('nock');
+
+// Simulate the API call and response
+test('should fetch data from the API', async () => {
+  // Mocking the API endpoint
+  const mockResponse = {
+    // Your expected response body here
+    timestamp: '2023-12-12',
+    // Other relevant fields
+  };
+
+  // Define the mocked API endpoint and the response
+  nock('https://api.example.com')
+    .get('/your/api/endpoint')
+    .reply(200, mockResponse);
+
+  // Your actual function that calls the API
+  const fetchDataFromAPI = async () => {
+    const response = await fetch('https://api.example.com/your/api/endpoint');
+    return response.json();
+  };
+
+  // Call the function and check if it returns a 200 status
+  const data = await fetchDataFromAPI();
+  expect(data).toEqual(mockResponse);
+});
