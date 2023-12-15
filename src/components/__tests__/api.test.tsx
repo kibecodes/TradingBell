6490f1f5b43078523f5d37323649d7dcb7d109d4
Jonathan Kibet <106477223+kibecodes@users.noreply.github.com>
2023-12-15 11:20:37 +0300

    api.test pass but still get error 403 from fetch button

--- src/components/__tests__/api.test.tsx
+++ src/components/__tests__/api.test.tsx
@@ -1,7 +1,6 @@
 import nock from 'nock';
 
 test('should fetch data from the API', async () => {
-  // Mocking the API endpoint
   const mockResponse = {
     "ticker": "AAPL",
     "queryCount": 1,
@@ -20,23 +19,21 @@ test('should fetch data from the API', async () => {
         }
     ],
     "status": "OK",
-    "request_id": "ca1b28db567342cb9a5f344625f3f538",
+    /* "request_id": "", */
     "count": 1
   };
 
-  // Define the mocked API endpoint and the response
   nock('https://api.polygon.io')
     .get('/v2/aggs/ticker/AAPL/range/1/day/2023-01-09/2023-01-09?apiKey=tWerjbnMMo3aH2xOpsTBVMx50KfE2F7U')
     .reply(200, mockResponse);
 
-  // Your actual function that calls the API
   const fetchDataFromAPI = async () => {
     const response = await fetch('https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/day/2023-01-09/2023-01-09?apiKey=tWerjbnMMo3aH2xOpsTBVMx50KfE2F7U');
     return response.json();
   };
 
-  // Call the function and check if it returns a 200 status
   const data = await fetchDataFromAPI();
+  delete data.request_id;
   expect(data).toEqual(mockResponse);
 });
 
