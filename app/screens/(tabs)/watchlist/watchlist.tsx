7568516c56d62ba3b69c657684cf6511c239621b
Jonathan Kibet <106477223+kibecodes@users.noreply.github.com>
2024-04-10 18:38:54 +0300

    minor changes

--- app/screens/(tabs)/watchlist/watchlist.tsx
+++ app/screens/(tabs)/watchlist/watchlist.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Pressable, ScrollView } from 'react-native';
 
-import { CurrencyPairLogo } from './components/currencyPairLogo';
 import {
   OrderCard,
   OrderLogo,
@@ -13,16 +12,19 @@ import { Line } from '../../../utils/components/line.styles';
 import ModalComponent from '../../modal/[modal]';
 import ModalScreen from '../../modal/modal.component';
 
-interface ApiResponse {
+interface FetchedResult {
+  o: number;
+  c: number;
+  v: number;
+}
+interface FetchedResponse {
   ticker: string;
-  results: [{ o: number; v: number; c: number }];
+  request_id: string;
+  results: [FetchedResult];
 }
 
 export default function Watchlist() {
-  const [fetchedData, setFetchedData] = useState<ApiResponse>({
-    ticker: '',
-    results: [{ o: 0, v: 0, c: 0 }],
-  });
+  const [fetchedData, setFetchedData] = useState<FetchedResponse[]>([]);
   const [modalVisible, setModalVisible] = useState(false);
   const theme = useTheme();
 
@@ -33,25 +35,27 @@ export default function Watchlist() {
     setModalVisible(false);
   };
 
-  async function fetchData() {
-    const apiBaseUrl = 'https://api.polygon.io';
-    try {
-      const response = await fetch(
-        `${apiBaseUrl}/v2/aggs/ticker/C:EURUSD/range/1/day/2023-01-09/2023-01-09?adjusted=true&sort=asc&limit=120&apiKey=tWerjbnMMo3aH2xOpsTBVMx50KfE2F7U`,
-      );
-      const data: ApiResponse = await response.json();
-      console.log(data);
-      const { ticker, results } = data;
-      const destructuredData = { ticker, results };
-      console.log(ticker, results);
-      setFetchedData(destructuredData);
-    } catch (error: unknown) {
-      throw new Error('Problem fetching data !');
-    }
-  }
   useEffect(() => {
-    fetchData().catch(() => {
-      throw new Error('Failed to fetch data !');
+    async function fetchData() {
+      const currencyPairs = ['EURUSD', 'USDJPY', 'GBPUSD', 'AUDUSD', 'USDCAD'];
+      const apiBaseUrl = 'https://api.polygon.io';
+      try {
+        const fetchedData = await Promise.all(
+          currencyPairs.map(async (currencyPair) => {
+            const response = await fetch(
+              `${apiBaseUrl}/v2/aggs/ticker/C:${currencyPair}/range/1/day/2023-01-09/2023-01-09?adjusted=true&sort=asc&limit=120&apiKey=tWerjbnMMo3aH2xOpsTBVMx50KfE2F7U`,
+            );
+            const data: FetchedResponse = await response.json();
+            return data;
+          }),
+        );
+        setFetchedData(fetchedData);
+      } catch (error: unknown) {
+        console.error('Problem fetching data:', error);
+      }
+    }
+    fetchData().catch((error) => {
+      console.error('Problem fetching data:', error);
     });
   }, []);
 
@@ -67,53 +71,53 @@ export default function Watchlist() {
         }}
       >
         <Pressable onPress={openModal}>
-          <OrderCard style={{ backgroundColor: theme.colors.mainForeground }}>
-            <ModalComponent
-              isVisible={modalVisible}
-              closeModal={closeModal}
-              modalContent={<ModalScreen />}
-            />
-            <OrderLogo>
-              <CurrencyPairLogo
-                logoSource={{
-                  uri: 'https://s3-alpha-sig.figma.com/img/c18c/b456/64d41644334580c86d73fb53476d19de?Expires=1698624000&Signature=cou~7FWRMICx1TnD8QrC6YE-w4T-hQ0I7Avv-hJk~D9kq0j52-tkUglJ33mjBVlMfW~Gabbc0TBBQknJX4sNDgYRjP8fciJKaLQ674JCEhgXhsNGL0OMAr1UOsYz71RDZ8tJ1LZycoa3-l92H8gX521BgNjKuxNsd1zxGh1Ue1NaVPDPnc-XVFwRNLjcXwsr6Wrk4XBLtYdIMBL42UZgcXTBDfBxDDnbfPbpmW53~lTMkRdCGx1EeVAEoNkmj11ahxw~kc3b3ApHsWvTf1MFpeoMSzs3coxEU9QLmy-zrT0unOwDBdcjVfp4o-6gY8SeiFByqnPYIYLOGWAqCfTmQA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
-                }}
-                overlaySource={{
-                  uri: 'https://s3-alpha-sig.figma.com/img/79f4/10d9/1c627386e08a3bcd6b6bd064204cb50d?Expires=1698624000&Signature=APfQp6MQ6i6rIR6N~hvgkLkW5U3Y-qVoiJj-EPGhvZ-FJEC0tYNdUoL6Sk04ubwuS0H-BFcHuGPpfgNcU5FNASvD5uUbMsDbbM7dLhgf9J9lAKgJYu314D4gCsQhVgG~0SZfar2v5s7K7sw5IsIInLhje3uJ3NKEnIr-t~XoLlHXCfUc5Q~MJD6fUeYPWHW6ssY74Sjtmf1xM2~U0kewNwvE0A-kRmwFq3wRZxYZs3CO241OjqOnyePXN8PoDovpwMa1EFA5obzbwTQrVPQ7IdAE3g5O7HQ4cHD1zUdjy7vpZZM2K1hEP0J1vp6W1uGG6NKRYj3Npg0bqjhnO1ua~w__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
-                }}
-              />
-            </OrderLogo>
-            <Order>
-              <Text
-                style={{
-                  color: theme.colors.white,
-                  fontSize: theme.textVariants.trade.fontSize,
-                }}
+          {fetchedData.map(({ request_id, ticker, results }) =>
+            results.map((result) => (
+              <OrderCard
+                key={request_id}
+                style={{ backgroundColor: theme.colors.mainForeground }}
               >
-                {fetchedData.ticker}
-              </Text>
-              <Text
-                style={{
-                  color: theme.colors.grayText,
-                  fontSize: theme.textVariants.tradeInfo.fontSize,
-                }}
-              >
-                EUR/USD
-              </Text>
-            </Order>
-            <OrderNumbers>
-              <Text
-                style={{
-                  color: theme.colors.white,
-                  fontSize: theme.textVariants.trade.fontSize,
-                  fontWeight: '800',
-                }}
-              >
-                {fetchedData.results[0].o}
-              </Text>
-              <Text style={{ color: theme.colors.redPrimary }}>{}</Text>
-            </OrderNumbers>
-          </OrderCard>
+                <ModalComponent
+                  isVisible={modalVisible}
+                  closeModal={closeModal}
+                  modalContent={<ModalScreen />}
+                />
+                <OrderLogo></OrderLogo>
+                <Order>
+                  <Text
+                    style={{
+                      color: theme.colors.white,
+                      fontSize: theme.textVariants.trade.fontSize,
+                    }}
+                  >
+                    {ticker}
+                  </Text>
+                  <Text
+                    style={{
+                      color: theme.colors.grayText,
+                      fontSize: theme.textVariants.tradeInfo.fontSize,
+                    }}
+                  >
+                    Volume traded: {result.v}
+                  </Text>
+                </Order>
+                <OrderNumbers>
+                  <Text
+                    style={{
+                      color: theme.colors.white,
+                      fontSize: theme.textVariants.trade.fontSize,
+                      fontWeight: '800',
+                    }}
+                  >
+                    open: {result.o}
+                  </Text>
+                  <Text style={{ color: theme.colors.redPrimary }}>
+                    close: {result.c}
+                  </Text>
+                </OrderNumbers>
+              </OrderCard>
+            )),
+          )}
         </Pressable>
         <Line style={{ backgroundColor: theme.colors.linePrimary }} />
       </Box>
