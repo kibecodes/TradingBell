b83f7c87a9293022bad002fcf61bc1f2a4d1fbf1
Jonathan Kibet <106477223+kibecodes@users.noreply.github.com>
2024-04-20 22:30:51 +0300

    success storing and loading state in secureStore

--- app/screens/(tabs)/watchlist/watchlist.tsx
+++ app/screens/(tabs)/watchlist/watchlist.tsx
@@ -1,3 +1,4 @@
+import * as SecureStore from 'expo-secure-store';
 import React, { useEffect, useState } from 'react';
 import { Pressable, ScrollView } from 'react-native';
 
@@ -23,10 +24,16 @@ interface FetchedResponse {
   results: [FetchedResult];
 }
 
+interface StorageTypes {
+  STORE_KEY: string;
+  fetchedData: FetchedResponse[];
+}
+
 export default function Watchlist() {
   const [fetchedData, setFetchedData] = useState<FetchedResponse[]>([]);
   const [modalVisible, setModalVisible] = useState(false);
   const theme = useTheme();
+  const STORE_KEY = 'fetchedData';
 
   const openModal = () => {
     setModalVisible(true);
@@ -35,31 +42,62 @@ export default function Watchlist() {
     setModalVisible(false);
   };
 
-  useEffect(() => {
-    async function fetchData() {
-      const currencyPairs = ['EURUSD', 'USDJPY', 'GBPUSD', 'AUDUSD', 'USDCAD'];
-      const apiBaseUrl = 'https://api.polygon.io';
-      try {
-        const fetchedData = await Promise.all(
-          currencyPairs.map(async (currencyPair) => {
-            const response = await fetch(
-              `${apiBaseUrl}/v2/aggs/ticker/C:${currencyPair}/range/1/day/2023-01-09/2023-01-09?adjusted=true&sort=asc&limit=120&apiKey=tWerjbnMMo3aH2xOpsTBVMx50KfE2F7U`,
-            );
-            const data: FetchedResponse = await response.json();
-            return data;
-          }),
-        );
-        setFetchedData(fetchedData);
-      } catch (error: unknown) {
-        console.error('Problem fetching data:', error);
-      }
+  async function save({ STORE_KEY, fetchedData }: StorageTypes) {
+    const serializedData = JSON.stringify(fetchedData);
+    await SecureStore.setItemAsync(STORE_KEY, serializedData);
+  }
+
+  async function load() {
+    const loadedData = await SecureStore.getItemAsync(STORE_KEY);
+
+    if (fetchedData.length === 0 || typeof loadedData !== 'string') {
+      await fetchData()
+        .then((data) => {
+          setFetchedData(data);
+          return data;
+        })
+        .then((data) => save({ STORE_KEY, fetchedData: data }))
+        .catch((error) => console.log(error));
+    } else if (typeof loadedData == 'string' && fetchedData.length > 0) {
+      const parsedData: FetchedResponse[] = JSON.parse(loadedData);
+      setFetchedData(parsedData);
     }
-    fetchData().catch((error) => {
-      console.error('Problem fetching data:', error);
+  }
+
+  async function fetchData(): Promise<FetchedResponse[]> {
+    const currencyPairs = ['EURUSD', 'USDJPY', 'GBPUSD', 'AUDUSD', 'USDCAD'];
+    const apiBaseUrl = 'https://api.polygon.io';
+    try {
+      const fetchedData = await Promise.all(
+        currencyPairs.map(async (currencyPair) => {
+          const response = await fetch(
+            `${apiBaseUrl}/v2/aggs/ticker/C:${currencyPair}/range/1/day/2023-01-09/2023-01-09?adjusted=true&sort=asc&limit=120&apiKey=tWerjbnMMo3aH2xOpsTBVMx50KfE2F7U`,
+          );
+          const data: FetchedResponse = await response.json();
+          return data;
+        }),
+      );
+      setFetchedData(fetchedData);
+      return fetchedData;
+    } catch (error: unknown) {
+      console.error('Error fetching data:', error);
+    }
+    return fetchedData;
+  }
+
+  async function checkForStoredData() {
+    await load().catch(() => {
+      throw new Error('Houston, we have a problem !');
+    });
+  }
+
+  useEffect(() => {
+    checkForStoredData().catch((error) => {
+      console.log('Error displaying fetched data:', error);
     });
   }, []);
 
-  return fetchedData ? (
+  return (
     <ScrollView>
       <Box
         data-testID="watchlist-component"
@@ -122,7 +160,5 @@ export default function Watchlist() {
         <Line style={{ backgroundColor: theme.colors.linePrimary }} />
       </Box>
     </ScrollView>
-  ) : (
-    <Text>No fetched data at the moment !</Text>
   );
 }
